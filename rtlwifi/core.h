cb06d99d72ab666c0624615389786bc92d9b243f
Larry Finger <Larry.Finger@lwfinger.net>
2014-09-26 16:40:27 -0500

    rtlwifi: rtl8188ee: rtl8192com: rtl8192cu: rtl8192ee: rtl8723ae: rtl87323be: rtl8821ae: Use common cmd_send_packet
    A locking problem was found in routine _rtl92ee_cmd_send_packet() that led
    to system freezes. Upon inspection, several drivers had the same problem;
    however, the routines all used the same code. The common code has been
    moved into rtlwifi.
    
    Signed-off-by: Larry Finger <Larry.Finger@lwfinger.net>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>

--- rtlwifi/core.h
+++ rtlwifi/core.h
@@ -42,4 +42,5 @@ void rtl_rfreg_delay(struct ieee80211_hw *hw, enum radio_path rfpath, u32 addr,
 		     u32 mask, u32 data);
 void rtl_bb_delay(struct ieee80211_hw *hw, u32 addr, u32 data);
 void rtl_mac80211_init(struct ieee80211_hw *hw);
+bool rtl_cmd_send_packet(struct ieee80211_hw *hw, struct sk_buff *skb);
 #endif
