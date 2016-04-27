2d9163f49e5d2e486e30e0758625c0708178c8c3
Christian Lamparter <chunkeey@googlemail.com>
2016-04-27 17:06:02 +0200

    rtlwifi: rtl8192se: Fix module parameter initialization
    Two of the module parameter descriptions weren't linked up.
    In addition the value for software encryption is not transferred to
    the locations used by the driver.
    
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>

--- rtlwifi/rtl8192su/sw.c
+++ rtlwifi/rtl8192su/sw.c
@@ -67,6 +67,8 @@ static int rtl92su_init_sw_vars(struct ieee80211_hw *hw)
 	rtlpriv->psc.inactiveps = rtlpriv->cfg->mod_params->inactiveps;
 	rtlpriv->psc.swctrl_lps = rtlpriv->cfg->mod_params->swctrl_lps;
 	rtlpriv->psc.fwctrl_lps = rtlpriv->cfg->mod_params->fwctrl_lps;
+	rtlpriv->cfg->mod_params->sw_crypto =
+		rtlpriv->cfg->mod_params->sw_crypto;
 	if (!rtlpriv->psc.inactiveps)
 		pr_info("Power Save off (module option)\n");
 	if (!rtlpriv->psc.fwctrl_lps)
@@ -429,3 +431,5 @@ module_param_named(swenc, rtl92su_mod_params.sw_crypto, bool, 0444);
 module_param_named(debug, rtl92su_mod_params.debug, int, 0444);
 MODULE_PARM_DESC(swenc, "Set to 1 for software crypto (default 0)\n");
 MODULE_PARM_DESC(debug, "Set debug level (0-5) (default 0)");
+MODULE_PARM_DESC(swlps, "Set to 1 to use SW control power save (default 0)\n");
+MODULE_PARM_DESC(fwlps, "Set to 1 to use FW control power save (default 0)\n");
